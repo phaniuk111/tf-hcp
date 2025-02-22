de3e09ff15d639ac882fd812f7cd2dffc3885d57
phaniuk111 <157854513+phaniuk111@users.noreply.github.com>
2025-02-22 14:42:35 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_instance" "web" {
   ami           = "ami-0431ce328feea0d11"  # Direct AMI ID for Amazon Linux 2 in eu-west-2
   instance_type = "t2.micro"
@@ -6,3 +7,4 @@ resource "aws_instance" "web" {
     Name = "web-server"
   }
 }
+*/
