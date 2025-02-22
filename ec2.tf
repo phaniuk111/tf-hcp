d80738d4eeac618a79780e3eda71a65913ea01a6
phaniuk111 <157854513+phaniuk111@users.noreply.github.com>
2025-02-22 14:34:09 +0000

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0431ce328feea0d11"  # Direct AMI ID for Amazon Linux 2 in eu-west-2
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "web-server"
+  }
+}
